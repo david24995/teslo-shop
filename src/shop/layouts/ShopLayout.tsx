ab0f40946434b14c08c5077d2daf5dfd09d4fe8f
David Huamaccto Chate <davidhuamaccto24995@gmail.com>
2026-07-22 23:13:46 -0500

    Agregando partes del proyecto, Footer, Jumbotron, etc

--- src/shop/layouts/ShopLayout.tsx
+++ src/shop/layouts/ShopLayout.tsx
@@ -1,10 +1,13 @@
 import { Outlet } from 'react-router';
+import CustomHeader from '../components/CustomHeader';
+import CustomFooter from '../components/CustomFooter';
 
 const ShopLayout = () => {
   return (
-    <div>
-      ShopLayout
+    <div className="min-h-screen bg-background">
+      <CustomHeader />
       <Outlet />
+      <CustomFooter />
     </div>
   );
 };
